f719784e42e75501cbae83bd912ebfdfe5c105c4
Nikkoola22 <nikkoola@gmail.com>
2025-09-13 09:03:26 +0200

    Fix chemin base pour Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: './', // ← pour Vercel
+  // Si on déploie sur GitHub Pages, on garde /gruuuu/
+  // Sinon (local ou Vercel), on met "./"
+  base: process.env.GITHUB_PAGES ? '/gruuuu/' : './',
 });
